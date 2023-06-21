7d4295ab8678941ef0a5d38a352e5915826b64e7
Rahul <rahul39khanal@gmail.com>
2023-06-21 18:58:31 +0545

    db

--- Backend/Database/database.js
+++ Backend/Database/database.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
-  database: "tms",
+  database: "tmsproject",
 });
 
 // Connect to the MySQL server
--- Backend/Routes/route.js
+++ Backend/Routes/route.js
@@ -1,19 +1,23 @@
 const express = require("express");
 const officeTime = require("../Controller/officeTime");
-const AuthController = require("../auth/auth");
+// const AuthController = require("../auth/auth");
 const path = require("path");
 const router = express.Router();
 
 let filePath;
 router.get("/", (req, resp) => {
-  filePath = path.join(__dirname,"/frontend/HTML/index.html")
+  filePath = path.join(__dirname, "/frontend/HTML/index.html");
   resp.sendFile(filePath);
 });
 router.get("/home", (req, resp) => {
-  filePath = path.join(__dirname,"/frontend/HTML/Home.html")
+  filePath = path.join(__dirname, "/frontend/HTML/Home.html");
   resp.sendFile(filePath);
 });
-router.post("/login", AuthController("admin"), (req, res) => {});
+router.get("/createproject", (req, resp) => {
+  filePath = path.join(__dirname, "/frontend/HTML/createProject.html");
+  resp.sendFile(filePath);
+});
+// router.post("/login", AuthController("admin"), (req, res) => {});
 
 router.get("/login", (req, resp) => {
   const filepath = path.join(__dirname);
