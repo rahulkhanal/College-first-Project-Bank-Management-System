daf36a3dc40eebc7c40a4c699d4eb5e165b3bcf4
Rahul Khanal <rahul39khanal@gmail.com>
2023-11-17 15:18:15 +0545

    status field added

--- Backend/Model/project.js
+++ Backend/Model/project.js
@@ -1,89 +1,77 @@
 const connection = require("../Database/database");
 module.exports = {
-    insertProject: (insertProject = (req, resp) => {
-      const { name, start_date, end_date, department } = req.body;
-      
-      connection.query(
-        "insert into Project(Name,Start_date,End_date,Department)VALUES(?,?,?,?)",
-        [name, start_date, end_date, department],
-        (err, res) => {
-          if (err) {
-            resp.json({
-              status: 500,
-              msg: err,
-            });
-          } else {
-            resp.json({
-              status: 200,
-              msg: res,
-            });
-          }
-        }
-      );
-    }),
-  
-    searchProject: (req, resp) => {
-        const id = req.params.id;
-        connection.query("SELECT * FROM Project WHERE Id = ?", [id], (err, res) => {
-          if (err) {
-            throw err;
-          } else {
-            if (res.length > 0) {
-              resp.render("updateProject", { data: res[0], id: id });
-            }
-          }
-        });
-      },
+  insertProject: (insertProject = (req, resp) => {
+    const { name, start_date, end_date } = req.body;
 
-    updateProject: (updateProject = (req, resp) => {
-      const { name, start_date, end_date, department } = req.body;
-      const { id } = req.params;
-      connection.query(
-        "UPDATE Project SET Name=?,Start_date=?, End_date=?, Department=? WHERE Id=?",[
-          name, start_date, end_date, department, id
-        ],
-        (err, res) => {
-          if (err) {
-            resp.json({
-              status: 500,
-              msg: err,
-            });
-          } else {
-            resp.json({
-              status: 200,
-              msg: res,
-            });
-          }
-        }
-      );
-    }),
-  
-    deleteProject: (deleteProject = (req, resp) => {
-     
-      const { id } = req.params;
-      console.log(req.params);
-      connection.query("DELETE FROM Project WHERE Id=? ", [id], (err, res) => {
+    connection.query(
+      "insert into Project(Name,Start_date,End_date)VALUES(?,?,?)",
+      [name, start_date, end_date],
+      (err, res) => {
         if (err) {
-          throw err;
+          resp.status(500).json({ message: "Invalid request" });
         } else {
-          
-          resp.status(200).json({
+          resp.status(200).json({ message: "Successfully created a project" });
+        }
+      }
+    );
+  }),
+
+  searchProject: (req, resp) => {
+    const id = req.params.id;
+    connection.query("SELECT * FROM Project WHERE Id = ?", [id], (err, res) => {
+      if (err) {
+        throw err;
+      } else {
+        if (res.length > 0) {
+          resp.render("updateProject", { data: res[0], id: id });
+        }
+      }
+    });
+  },
+
+  updateProject: (updateProject = (req, resp) => {
+    const { name, start_date, end_date, department } = req.body;
+    const { id } = req.params;
+    connection.query(
+      "UPDATE Project SET Name=?,Start_date=?, End_date=?, Department=? WHERE Id=?",
+      [name, start_date, end_date, department, id],
+      (err, res) => {
+        if (err) {
+          resp.json({
+            status: 500,
+            msg: err,
+          });
+        } else {
+          resp.json({
+            status: 200,
             msg: res,
           });
         }
-      });
-    }),
-    readProject: (readProject = (req, resp) => {
-      
-      connection.query("SELECT * FROM Project ", (err, res) => {
-        if (err) {
-          throw err;
-        } else {
-             console.log(res);
-            resp.render("project", { data: res });
-        }
-      });
-    }),
-  
-  };
-  
+      }
+    );
+  }),
+
+  deleteProject: (deleteProject = (req, resp) => {
+    const { id } = req.params;
+    console.log(req.params);
+    connection.query("DELETE FROM Project WHERE Id=? ", [id], (err, res) => {
+      if (err) {
+        throw err;
+      } else {
+        resp.status(200).json({
+          msg: res,
+        });
+      }
+    });
+  }),
+  readProject: (readProject = (req, resp) => {
+    connection.query("SELECT * FROM Project ", (err, res) => {
+      if (err) {
+        throw err;
+      } else {
+        console.log(res);
+        resp.render("project", { data: res });
+      }
+    });
+  }),
+};
--- Backend/Routes/route.js
+++ Backend/Routes/route.js
@@ -5,10 +5,24 @@ const path = require("path");
 const { login } = require("../Controller/login");
 const authentication = require("../middlewares/autthentication");
 const authorization = require("../middlewares/authorization");
-const { createAccount, viewAccount, deleteAccount, searchAccount, updateAccount } = require("../Model/Account");
+const {
+  createAccount,
+  viewAccount,
+  deleteAccount,
+  searchAccount,
+  updateAccount,
+} = require("../Model/Account");
 const router = express.Router();
-const { createProjectLayer, deleteProjectLayer, updateProjectLayer } = require("../Controller/projectLayer");
-const { readProject, deleteProject, searchProject } = require("../Model/project");
+const {
+  createProjectLayer,
+  deleteProjectLayer,
+  updateProjectLayer,
+} = require("../Controller/projectLayer");
+const {
+  readProject,
+  deleteProject,
+  searchProject,
+} = require("../Model/project");
 //all the file
 
 router.get("/", (req, res) => {
@@ -26,25 +40,22 @@ router.get("/createUser", (req, res) => {
   res.render("createUser");
 });
 router.get("/user", viewAccount, (req, res) => {
-
-
   res.render("user");
 });
-router.get("/updateUser/:id", searchAccount, (req,resp)=>{
-  resp.render("updateUser.hbs")
-})
-router.get("/createproject", (req,resp)=>{
-resp.render("createProject.hbs")
-})
-router.get("/project", readProject,(req,resp)=>{
-  resp.render("project.hbs")
-  })
-router.get("/updateproject/:id", searchProject,(req,resp)=>{
-  resp.render("updateProject.hbs")
-})
+router.get("/updateUser/:id", searchAccount, (req, resp) => {
+  resp.render("updateUser.hbs");
+});
+router.get("/createproject", (req, resp) => {
+  resp.render("createProject.hbs");
+});
+router.get("/project", readProject, (req, resp) => {
+  resp.render("project.hbs");
+});
+router.get("/updateproject/:id", searchProject, (req, resp) => {
+  resp.render("updateProject.hbs");
+});
 //post
 router.post("/api/register", createAccount);
-
 
 //delete
 router.delete("/api/register/:id", deleteAccount);
@@ -54,12 +65,11 @@ router.post("/login", login);
 router.post("/storeTime", officeTime);
 
 //PUT method
-router.put("/api/updateAccount/:id", updateAccount)
+router.put("/api/updateAccount/:id", updateAccount);
 
- 
-router.delete("/project/:id", deleteProject)
-router.get("/project",readProject)
-router.post("/project", createProjectLayer)
-router.put("/project/:id", updateProjectLayer)
+router.delete("/project/:id", deleteProject);
+router.get("/project", readProject);
+router.post("/project", createProjectLayer);
+router.put("/project/:id", updateProjectLayer);
 
 module.exports = router;
